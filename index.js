5a6e354a69427310dc128048913e012439fdf53d
Bharat Balaji D <108548870+bharatbalajid@users.noreply.github.com>
2023-07-23 09:57:19 +0530

    test ecs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello world');
+  res.send('Hello world 2');
 });
 
 /* istanbul ignore next */
